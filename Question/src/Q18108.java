acaf669902fb1e561381118576075c76df311272
서정민 <wjdals8625@gmail.com>
2023-12-12 16:13:40 +0900

    백준 18108번
    18108번

---
+++ Question/src/Q18108.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Q18108 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        
+        int result = year-543;
+
+        System.out.println(result);
+    }
+}
