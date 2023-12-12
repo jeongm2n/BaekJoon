4bb8f925d78fd8bae91e3a176e540613b14f10e7
서정민 <wjdals8625@gmail.com>
2023-12-12 16:22:01 +0900

    백준 11382번 문제
    Int 타입으로 하니 런타임 에러 떠서 Long타입으로 변경

---
+++ Question/src/Q11382.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Q11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long one = sc.nextLong();
+        long two = sc.nextLong();
+        long three = sc.nextLong();
+
+        System.out.println((one+two+three));
+    }
+}
