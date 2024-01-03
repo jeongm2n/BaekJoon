4e3307eb541e50b9b06e4e0de7169c8cbc261a88
서정민 <wjdals8625@gmail.com>
2024-01-03 15:41:38 +0900

    백준 10798번 문제
    백준 10798번 문제  1트에 정답

---
+++ Question/src/Q10798.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q10798 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char arr[][] = new char[5][15];
+        for(int i=0; i<5; i++){
+            String a = sc.next();
+            for(int j=0; j<a.length(); j++){
+                arr[i][j] = a.charAt(j);
+            }
+        }
+
+        for(int i=0; i<15; i++){
+            for(int j=0; j<5; j++){
+                if(arr[j][i] != 0){
+                    System.out.print(arr[j][i]);
+                }
+            }
+        }
+
+    }    
+}
