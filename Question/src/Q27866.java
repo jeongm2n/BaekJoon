f0391401107721c683996c99bcba3ceba2baadff
서정민 <wjdals8625@gmail.com>
2024-02-07 14:33:14 +0900

    백준 27866번 문제
    런타임 에러 발생으로 2트만에 정답

---
+++ Question/src/Q27866.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int i = sc.nextInt();
+
+        char c = str.charAt(i-1);
+
+        System.out.println(c);
+    }    
+}
