526e4f403adaf538ff2a0401b064040433a1c6dd
서정민 <wjdals8625@gmail.com>
2024-02-07 14:37:39 +0900

    백준 2743번 문제
    1트에 정답

---
+++ Question/src/Q2743.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Q2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        String str = sc.next();
+
+        int l = str.length();
+
+        System.out.println(l);
+    }
+}
