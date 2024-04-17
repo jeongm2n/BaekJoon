1db68d9e7b08079a26944914450b33fe4f1bc463
서정민 <wjdals8625@gmail.com>
2024-04-17 14:07:55 +0900

    백준 11650번 2트에 정답

---
+++ Question/src/Q11650.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Q11650 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][2];
+
+        for(int i=0; i<n; i++){
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, (arr_1, arr_2) -> {
+
+			if(arr_1[0] == arr_2[0])  {
+				return arr_1[1] - arr_2[1];
+			}
+			else {
+				return arr_1[0] - arr_2[0];	
+			}
+		});
+
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
