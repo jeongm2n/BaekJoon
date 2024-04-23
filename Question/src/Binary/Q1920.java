4a1c9c3ed079eeb807463a4cdabc45c947bc538e
서정민 <wjdals8625@gmail.com>
2024-04-23 14:36:26 +0900

    폴더명 변경

--- Question/src/Binary/Q1920.java
+++ Question/src/Binary/Q1920.java
@@ -1,32 +1,34 @@
-package Conquer;
+package Binary;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Q10816 {
+public class Q1920 {
     public static void main(String[] args) throws IOException {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for(int i=0; i<n; i++){
-            int a = Integer.parseInt(st.nextToken());
-            if(!map.containsKey(a)) map.put(a,1);
-            else map.put(a, map.get(a)+1);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
 
         int m = Integer.parseInt(br.readLine());
+        int[] marr = new int[m];
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<m; i++){
-            int a = Integer.parseInt(st.nextToken());
-            if(!map.containsKey(a)) sb.append(0).append(" ");
-            else sb.append(map.get(a)).append(" ");
+            marr[i] = Integer.parseInt(st.nextToken());
+            if(Arrays.binarySearch(arr, marr[i])<0) sb.append(0).append("\n");
+            else sb.append(1).append("\n");
         }
+
         System.out.println(sb);
     }
 }
--- Question/src/Binary/Q2805.java
+++ Question/src/Binary/Q2805.java
@@ -1,4 +1,4 @@
-package Conquer;
+package Binary;
 
 import java.io.BufferedReader;
 import java.io.IOException;
