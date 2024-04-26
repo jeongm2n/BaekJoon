7713bbefb5f8283b485ba534b3b6ea9e90dcff97
서정민 <wjdals8625@gmail.com>
2024-04-26 12:35:35 +0900

    백준 17298번 6트에 정답

---
+++ Question/src/Stack/Q17298.java
@@ -0,0 +1,33 @@
+package Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Q17298 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i<n; i++){
+            while(!stack.isEmpty() && arr[stack.peek()]<arr[i]){
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()) arr[stack.pop()] = -1;
+
+        StringBuilder sb = new StringBuilder();
+        for(int i : arr) sb.append(i).append(" ");
+
+        System.out.println(sb);
+    }
+}
