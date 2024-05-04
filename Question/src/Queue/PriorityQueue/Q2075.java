227749ab2dcf9fa4728bbf6b5ef76ffa5f1e6c97
서정민 <wjdals8625@gmail.com>
2024-05-04 10:13:29 +0900

    백준 2075번 1트에 정답!

---
+++ Question/src/Queue/PriorityQueue/Q2075.java
@@ -0,0 +1,29 @@
+package Queue.PriorityQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Q2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringTokenizer st;
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) pq.offer(Long.parseLong(st.nextToken()));
+        }
+
+        long result = 0;
+        for(int i=0; i<n; i++){
+            result = pq.poll();
+        }
+
+        System.out.println(result);
+    }
+}
