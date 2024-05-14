aa9930651b7edf731d4569b5f5595c6890d2e632
서정민 <wjdals8625@gmail.com>
2024-05-14 11:03:13 +0900

    백준 15809번 주석

--- Question/src/Set/Q15809.java
+++ Question/src/Set/Q15809.java
@@ -11,14 +11,17 @@ public class Q15809 {
     static int[] army, parent;
 
     public static int find(int x){
+        //각 나라의 부모 나라(동맹을 맺었거나 속국이 된 경우에는 해당 나라가, 동맹도 속국도 아니면 자기 자신이 값) 찾는 함수
         if(x==parent[x]) return x;
         return parent[x] = find(parent[x]);
     }
 
-    public static void union(int x, int y){
+    public static void union(int x, int y){ //동맹 맺을 때의 메서드
         x = find(x);
         y = find(y);
 
+        //국가 번호가 더 작은 국가가 부모가 되도록 설정 ex) 1 - 3 국가가 동맹을 맺는다면 3의 부모가 1이 되도록
+        //부모 국가에 자식 국가의 병력을 합쳐주고 자식 국가의 값은 0으로 변경 
         if(x>y){
             parent[x]=y;
             army[y] += army[x];
@@ -30,19 +33,23 @@ public class Q15809 {
         }
     }
 
-    public static void war(int x, int y){
+    public static void war(int x, int y){ //전쟁 할 때의 메서드
+        // 각 국가의 부모 국가를 찾아옴
         x = find(x);
         y = find(y);
-
-        if(army[x]>army[y]){
+        
+        //부모 국가의 병력을 비교해서 더 작은 쪽이 큰 쪽의 속국이 됨
+        //동맹때와 똑같이 병력이 작은 쪽의 부모를 큰 쪽의 부모로 변경
+        if(army[x]>army[y]){ 
             parent[y] = x;
-            army[x] -= army[y];
-            army[y] = 0;
+            army[x] -= army[y]; //이긴쪽의 병력에서 속국이 된 곳의 병력을 뺌
+            army[y] = 0; //속국이 된 곳의 병력은 0으로 변경
         }else if(army[y]>army[x]){
             parent[x] = y;
             army[y] -= army[x];
             army[x] = 0;
-        }else{
+        }else{ //두 나라의 병력이 같다면 두 나라 다 멸망해야 함
+            //두 나라의 부모 노드와 병력을 모두 0으로 바꿔줌
             parent[x] = 0;
             parent[y] = 0;
             army[x] = 0;
@@ -92,7 +99,7 @@ public class Q15809 {
         if(arr.size()!=0){
             for(int x : arr) sb.append(x + " ");
         }
-        
+
         System.out.println(sb);
 
     }
