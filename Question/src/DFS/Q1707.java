4c3703ed06c80899fedbfe4abf47223848ee57a0
서정민 <wjdals8625@gmail.com>
2024-05-23 18:03:09 +0900

    백준 1707번 3트에 정답

--- Question/src/DFS/Q1707.java
+++ Question/src/DFS/Q1707.java
@@ -7,13 +7,20 @@ import java.util.*;
 
 public class Q1707 {
     static ArrayList<Integer>[] graph;
-    static boolean[] visited;
+    static int red = 1;
+    static int blue = -1;
+    static int[] colors;
+    static boolean check;
 
-    static void dfs(int s){
-        visited[s] = true;
+    static void dfs(int s, int color){
+        colors[s] = color;
 
         for(int v : graph[s]){
-            if(!visited[v]) dfs(v);
+            if(colors[v]==color){
+                check = false;
+                return;
+            }
+            if(colors[v]==0) dfs(v, color*(-1));
         }
     }
 
@@ -29,11 +36,16 @@ public class Q1707 {
             
             int V = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
+            
+            check = true;
 
             graph = new ArrayList[V+1];
-            visited = new boolean[V+1];
+            colors = new int[V+1];
 
-            for(int i=1; i<=V; i++) graph[i] = new ArrayList<Integer>();
+            for(int i=1; i<=V; i++){
+                graph[i] = new ArrayList<Integer>();
+                colors[i] = 0;
+            }
 
             for(int i=0; i<E; i++){
                 st = new StringTokenizer(br.readLine());
@@ -42,17 +54,15 @@ public class Q1707 {
                 int v = Integer.parseInt(st.nextToken());
 
                 graph[u].add(v);
+                graph[v].add(u);
             }
-            int cnt = 0;
 
             for(int i=1; i<=V; i++){
-                if(!visited[i]){
-                    dfs(i);
-                    cnt++;
-                }
+                if(!check) break;
+                if(colors[i]==0) dfs(i, red);
             }
 
-            sb.append(cnt>1 ? "YES" : "NO").append("\n");
+            sb.append(check ? "YES" : "NO").append("\n");
         }
 
         System.out.println(sb);
