d9d45d45734adc70b4bb92e4e634a1653aef6cea
서정민 <wjdals8625@gmail.com>
2024-07-22 17:26:58 +0900

    백준 1987번 틀렸던거 고쳐서 맞춤

--- Question/src/DFS/Q1987.java
+++ Question/src/DFS/Q1987.java
@@ -3,38 +3,31 @@ package DFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q1987 {
     static char[][] graph;
-    static boolean[] alpha = new boolean[26];
+    static boolean[] visited;
 
     static int[] dx = {0,0,-1,1};
     static int[] dy = {-1,1,0,0};
 
-    static int r;
-    static int c;
-
-    static int result = 0;
+    static int r, c, max=0;
 
     static void dfs(int x, int y, int cnt){
-        if(alpha[graph[y][x]-'A']){
-            result = Math.max(result, cnt);
-            return;
-        }
-
-        alpha[graph[y][x]-'A'] = true;
+        visited[graph[y][x]-'A'] = true;
+        max = Math.max(max, cnt);
 
         for(int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
 
             if(nx>=0 && nx<c && ny>=0 && ny<r){
-                dfs(0,0,cnt+1);
+                if(visited[graph[ny][nx]-'A']) continue;
+                dfs(nx, ny, cnt+1);
+                visited[graph[ny][nx]-'A'] = false;
             }
         }
-
-        alpha[graph[y][x]-'A'] = false;
     }
 
     public static void main(String[] args) throws IOException {
@@ -46,13 +39,14 @@ public class Q1987 {
         c = Integer.parseInt(st.nextToken());
 
         graph = new char[r][c];
+        visited = new boolean[26];
 
         for(int i=0; i<r; i++){
             graph[i] = (br.readLine()).toCharArray();
         }
 
-        dfs(0,0,0);
+        dfs(0,0,1);
 
-        System.out.println(result);
+        System.out.println(max);
     }   
 }
