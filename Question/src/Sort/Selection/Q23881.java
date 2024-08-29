d08f6a4aa44c06ac35f911f498cd6f159ede79c6
서정민 <wjdals8625@gmail.com>
2024-08-29 16:12:44 +0900

    백준 23881번 6트에 정답, 문제 설명에 있는 수도코드대로 안했더니 틀렸던 거였음

--- Question/src/Sort/Selection/Q23881.java
+++ Question/src/Sort/Selection/Q23881.java
@@ -12,23 +12,44 @@ public class Q23881 {
 
     static void selection_sort(){
         int cnt = 0;
-        for(int left=0; left<N-1; left++){
-            int min = left;
-            for(int i=left+1; i<N; i++){
-                if(arr[i]<arr[min]) min = i;
+        for(int r=N-1; r>=1; r--){
+            int max = r;
+            for(int i=0; i<r; i++){
+                if(arr[i]>arr[max]) max=i;
             }
-            if(left!=min){
-                int tmp = arr[left];
-                arr[left] = arr[min];
-                arr[min] = tmp;
+            if(r!=max){
+                int tmp = arr[r];
+                arr[r] = arr[max];
+                arr[max] = tmp;
                 cnt++;
                 if(cnt==K){
-                    result = arr[left] + " " + arr[min];
+                    result = arr[max] + " " + arr[r];
                     return;
                 }
             }
         }
     }
+
+    // static void selection_sort(){ 기존 코드
+    //     int cnt = 0;
+    //     for(int left=0; left<N-1; left++){
+    //         int min = left;
+    //         for(int i=left+1; i<N; i++){
+    //             if(arr[i]<arr[min]) min = i;
+    //         }
+    //         if(left!=min){
+    //             int tmp = arr[left];
+    //             arr[left] = arr[min];
+    //             arr[min] = tmp;
+    //             cnt++;
+    //             if(cnt==K){
+    //                 result = arr[0] + " ";
+    //                 for(int j=1; j<N; j++) result += arr[j] + " ";
+    //                 return;
+    //             }
+    //         }
+    //     }
+    // }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
