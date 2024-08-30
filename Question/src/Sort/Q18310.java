d1089fd028d61dedef5064814df9c55e7eed03c4
서정민 <wjdals8625@gmail.com>
2024-08-30 15:01:19 +0900

    백준 18310번 2트에 정답

---
+++ Question/src/Sort/Q18310.java
@@ -0,0 +1,20 @@
+package Sort;
+
+import java.io.*;
+import java.util.*;
+
+public class Q18310 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[(N-1)/2]);
+    }   
+}
