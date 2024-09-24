2a3c243fb30a76f7cb4346d367dcfd229ebd8a54
서정민 <wjdals8625@gmail.com>
2024-09-24 14:03:53 +0900

    백준 5648번 4트에 정답 (입력 정수 조건 잘 안봐서 틀림)

--- Question/src/Sort/Q5648.java
+++ Question/src/Sort/Q5648.java
@@ -7,37 +7,25 @@ public class Q5648 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String line = br.readLine();
-        String[] splits = line.split(" ");
-        
-        int N = Integer.parseInt(splits[0]);
-        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Long> arr = new ArrayList<>();
         
         int cnt = 0;
         StringBuffer b;
-
-        if(splits.length>1){
-            for(int i=1; i<splits.length; i++){
-                b = new StringBuffer(splits[i]);
-                arr.add(Integer.parseInt(b.reverse().toString()));
+        int N = 1;
+        while(arr.size()<N){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            if(cnt==0) N = Integer.parseInt(st.nextToken());
+            while(st.hasMoreTokens()){
+                b = new StringBuffer(st.nextToken());
+                arr.add(Long.parseLong(b.reverse().toString()));
             }
-            cnt = splits.length-1;
-        }
-        
-        while(cnt<N){
-            line = br.readLine();
-            splits = line.split(" ");
-            for(String s : splits){
-                b = new StringBuffer(s);
-                arr.add(Integer.parseInt(b.reverse().toString()));
-            }
-            cnt += splits.length;
+            cnt++;
         }
 
         Collections.sort(arr);
 
         StringBuilder sb = new StringBuilder();
-        for(int i : arr) sb.append(i + "\n");
+        for(long i : arr) sb.append(i + "\n");
         System.out.println(sb);
     }
 }
