9abe715fa8aff89270ea89c36d466154d8b422eb
서정민 <wjdals8625@gmail.com>
2024-10-16 16:08:24 +0900

    백준 20551번 3트에 정답

--- Question/src/Sort/Q20551.java
+++ Question/src/Sort/Q20551.java
@@ -11,21 +11,19 @@ public class Q20551 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        long[] arr = new long[N];
-        for(int i=0; i<N; i++) arr[i] = Long.parseLong(br.readLine());
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(br.readLine());
         Arrays.sort(arr);
 
         StringBuilder sb = new StringBuilder();
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i=0; i<N; i++) if(!hm.containsKey(arr[i])) hm.put(arr[i], i);
 
         for(int i=0; i<M; i++){
-            long a = Long.parseLong(br.readLine());
+            int a = Integer.parseInt(br.readLine());
             int tmp = -1;
-            for(int j=0; j<N; j++){
-                if(a==arr[j]){
-                    tmp = j;
-                    break;
-                }
-            }
+            if(hm.containsKey(a)) tmp = hm.get(a);
             sb.append(tmp==-1 ? -1 : tmp).append("\n");
         }
 
