b8793d3365fc266891073cb453c94022ce492074
서정민 <wjdals8625@gmail.com>
2024-12-03 15:17:15 +0900

    백준 19952번 1트 런타임에러

---
+++ Question/src/BFS/Q19952.java
@@ -0,0 +1,91 @@
+package BFS;
+
+import java.io.*;
+import java.util.*;
+
+public class Q19952 {
+    static int T, H, W, O, F;
+    static int[][] graph;
+    static boolean[][] visited;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {-1,1,0,0};
+
+    static class Node{
+        int x, y, hp;
+        public Node(int x, int y, int hp){
+            this.x = x;
+            this.y = y;
+            this.hp = hp;
+        }
+    }
+
+    static String bfs(int sx, int sy, int ex, int ey){
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(sx, sy, F));
+        visited[sy][sx] = true;
+
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+            int x = cur.x;
+            int y = cur.y;
+            int hp = cur.hp;
+
+            if(x==ex && y==ey) return "잘했어!!";
+
+            for(int i=0; i<4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx<1 || ny<1 || nx>W || ny>H || visited[ny][nx]) continue;
+
+                if(graph[ny][nx]<=graph[y][x]){
+                    q.offer(new Node(nx, ny, hp-1));
+                    visited[ny][nx] = true;
+                }else{
+                    if(hp>=graph[ny][nx]-graph[y][x]){
+                        q.offer(new Node(nx, ny, hp-1));
+                        visited[ny][nx] = true;
+                    }
+                }
+            }
+        }
+        return "인성 문제있어??";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+
+        while(T-->0){
+            int sx, sy, ex, ey;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            H = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            O = Integer.parseInt(st.nextToken());
+            F = Integer.parseInt(st.nextToken());
+            sx = Integer.parseInt(st.nextToken());
+            sy = Integer.parseInt(st.nextToken());
+            ex = Integer.parseInt(st.nextToken());
+            ey = Integer.parseInt(st.nextToken());
+
+            graph = new int[H+1][W+1];
+            visited = new boolean[H+1][W+1];
+
+            for(int i=0; i<O; i++){
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                int l = Integer.parseInt(st.nextToken());
+
+                graph[y][x] = l;
+            }
+
+            sb.append(bfs(sx, sy, ex, ey)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
