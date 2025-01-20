5cfc5bd1b5995f1301ed11174aa48a76df77c3cd
서정민 <wjdals8625@gmail.com>
2025-01-20 15:14:49 +0900

    백준 13023번 4트 틀림

---
+++ Question/src/DFS/Q13023.java
@@ -0,0 +1,55 @@
+package DFS;
+
+import java.io.*;
+import java.util.*;
+
+public class Q13023 {
+    static int N, M, result=0;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    static void dfs(int s, int depth){
+        if(depth == 4){
+            result = 1;
+            return;
+        }
+
+        visited[s] = true;
+
+        for(int v : graph[s]){
+            if(!visited[v]){
+                dfs(v, depth+1);
+            }
+        }
+        
+        visited[s] = false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N];
+        visited = new boolean[N];
+
+        for(int i=0; i<N; i++) graph[i] = new ArrayList<>();
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for(int i=0; i<N; i++){
+            if(result!=1) dfs(i,0);
+        }
+
+        System.out.println(result);
+    }   
+}
