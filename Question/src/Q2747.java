a610fded66d9bc233e2f0b510b20cc4d44ad8701
서정민 <wjdals8625@gmail.com>
2025-03-20 15:04:03 +0900

    백준 2747번 1트에 정답!

---
+++ Question/src/Q2747.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Q2747 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[] result = new long[N+1];
+        result[1] = 1;
+
+        for(int i=2; i<=N; i++) result[i] = result[i-1] + result[i-2];
+
+        System.out.println(result[N]);
+    }
+}
