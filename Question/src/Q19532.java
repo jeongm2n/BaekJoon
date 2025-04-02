ff1a5afe6c0ba13f4681c70eb88e877d873d0b61
서정민 <wjdals8625@gmail.com>
2025-04-02 15:14:26 +0900

    백준 19532번 3트에 정답

---
+++ Question/src/Q19532.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Q19532 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        System.out.println((c*e- b*f) / (a*e - b*d) + " " + (c*d - a*f) / (b*d - a*e));
+
+    }
+}
