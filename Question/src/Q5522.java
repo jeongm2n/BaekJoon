4570f601bfe9166ff4dcf0c285f6ea9c34c03c8e
서정민 <wjdals8625@gmail.com>
2025-04-09 10:17:05 +0900

    백준 5522번 1트에 정답!

---
+++ Question/src/Q5522.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+
+public class Q5522 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum=0;
+        for(int i=0; i<5; i++){
+            int n = Integer.parseInt(br.readLine());
+            
+            sum += n;
+        }
+
+        System.out.println(sum);
+    }
+}
