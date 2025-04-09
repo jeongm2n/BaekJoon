6a53004733a331f1017fb421f542d8b23e7582d4
서정민 <wjdals8625@gmail.com>
2025-04-09 10:52:34 +0900

    백준 27433번 3트, 반복문으로 정답

--- Question/src/Q27433.java
+++ Question/src/Q27433.java
@@ -1,16 +1,18 @@
 import java.io.*;
 
 public class Q27433 {
-    static long factorial(long n){
-        if(n==1) return n;
-        return n * factorial(n-1);
-    }
+    // static long factorial(long n){
+    //     if(n==1) return n;
+    //     return n * factorial(n-1);
+    // }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        long result=1;
 
-        System.out.println(factorial(N));
+        for(int i=1; i<=N; i++) result *= i;
+        System.out.println(result);
     }
 }
