d9b7e47ce38288f5d1ea35225b6e02ddc3d7e554
서정민 <wjdals8625@gmail.com>
2025-04-10 16:26:49 +0900

    백준 24264번 2트에 정답

---
+++ Question/src/Q24264.java
@@ -0,0 +1,12 @@
+
+import java.io.*;
+
+public class Q24264 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        
+        System.out.println(N*N);
+        System.out.println(2);
+    }
+}
