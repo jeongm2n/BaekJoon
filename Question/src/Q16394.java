f60f5966e449b72b6a6e088b38d77d9858affba6
서정민 <wjdals8625@gmail.com>
2025-04-29 10:24:18 +0900

    백준 16394번 1트에 정답!

---
+++ Question/src/Q16394.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Q16394 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        System.out.println(N - 1946);
+    }
+}
