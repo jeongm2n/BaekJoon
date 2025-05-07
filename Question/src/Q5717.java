87a6ff75efad38a1d77c510202e9d35ea5925165
서정민 <wjdals8625@gmail.com>
2025-05-07 15:51:44 +0900

    백준 5717번 4트에 정답

---
+++ Question/src/Q5717.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q5717 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) { 
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a==0 && b==0) break;
+            System.out.println(a+b);
+        }
+    }    
+}
